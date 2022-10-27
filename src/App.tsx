8c5b2cda9e1930de98838e5cfa0ffbce045451cf
kyorotyan <e205702@ie.u-ryukyu.ac.jp>
2022-10-27 16:49:28 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,16 @@
 import React,{ useState} from 'react';
 import './App.css';
 
+type Record = {
+  title: string;
+  id: number;
+  body: string;
+};
+
 function App() {
-  const [inputvalue, setInputValue] = useState("");
   const [records, setRecords] = useState<Record[]>([]);
 
-
-  type Record = {
-    title: string;
-    id: number;
-    checked: boolean;
-    inputvalue: string;
-  };
-
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-
-    const newRecod: Record = {
-      inputvalue: inputvalue,
-      id: records.length,
-      checked: false,
-      title: ""
-    }
-
-    setRecords([newRecod, ...records]);
-    setInputValue("");
-  };
-
-    const handleEdit = (id:number, inputvalue: string) => {
-      const newRecod = records.map((record) => {
-        if(record.id === id){
-          record.inputvalue = inputvalue;
-        }
-        return record;
-      })
-
-      setRecords(newRecod);
-    };
-
-    const handleChecked = (id: number, checked: boolean) => {
-      const newRecod = records.map((record) => {
-        if(record.id === id){
-          record.checked = !checked;
-        }
-        return record;
-    });
-
-    setRecords(newRecod);
-  };
-
-
-  const printHelloWorld = () => {
+  const fetchPost = () => {
     const fetchRecordUrl = 'https://jsonplaceholder.typicode.com/posts/' + (records.length + 1);
     fetch(fetchRecordUrl, {
       method: 'GET',
@@ -64,12 +23,10 @@ function App() {
       })
       .then((json) => {
         const newRecod: Record = {
-          inputvalue: json!.title,
           id: records.length,
-          checked: json!.completed,
           title: json!.title,
-
-        }
+          body: json!.body
+        };
     
         setRecords([newRecod, ...records]);
       });
@@ -81,15 +38,14 @@ function App() {
     <div className="App">
      <div>
        <h2>Koki Blog</h2>
-       <form onSubmit={(e) => handleSubmit(e)}>
-       </form>
-       <button onClick={() => printHelloWorld()}>Get Post</button>
+          <button onClick={() => fetchPost()}>Get Post</button>
        <ul className='recordList'>
          {records.map((record) => (
-          <li key={record.id}>
-            <input  type='text'  onChange={(e) => handleEdit(record.id, e.target.value)} className="inputText" value={record.inputvalue} disabled={record.checked}/>
-            <input type='checkbox'  checked={record.checked} onChange={() => handleChecked(record.id, record.checked)}/>
-          </li>
+            <div className='record' key={record.id}>
+              <h3>{record.title}</h3>
+              <hr></hr>
+              <p>{record.body}</p>
+            </div>
          ))}
        </ul>
      </div>
